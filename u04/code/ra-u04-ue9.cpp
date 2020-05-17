26f9d3cd2de36fb0c72351f6ecb1cea0e86968ba
Sun Yudong <sunjerry019@gmail.com>
2020-05-17 10:04:44 +0800

    u04 abgabe

---
+++ u04/code/ra-u04-ue9.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+bool nor(bool a, bool b) { return !(a | b); }
+
+int main(int argc, char* argv[])
+{
+	std::vector <int (*)(int,int,int,int)> funk
+	{
+		[](int a, int b, int c, int d) -> int { return (a|b|c|d) & (a|b|c|!d) & (a|b|!c|!d) & (a|!b|!c|d) & (!a|b|c|!d) & (!a|b|!c|!d); },
+		[](int a, int b, int c, int d) -> int { return a&!b&c | a&b&d | !a&!b&!c; },
+	};
+	int n = funk.size();
+	printf("A B C ");
+	for (int u = 0; u < n; u ++) printf(" %d", u + 1);
+	printf("\n\n");
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			for (int k = 0; k < 2; k++) {
+				for (int l = 0; l < 2; l++) {
+					printf("%d %d %d %d ", i, j, k, l);
+					for (int u = 0; u < n; u ++) printf(" %d", funk[u](i,j,k,l));
+					printf("\n");
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+// https://stackoverflow.com/a/31643380
+// int (*funk[]) (int a, int b, int c) = 
+
+// std::string outputString = "%d %d %d";
+// for (int i = 0; i < n; i++)
+// {
+// 	outputString += " %d";
+// }
+// outputString.c_str()
