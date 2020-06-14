68fbf198dd2f3d7578cc22860b3f4c19f56783c0
Sun Yudong <sunjerry019@gmail.com>
2020-06-15 07:17:51 +0800

    u07

--- u07/binarytodecimal.s
+++ u07/binarytodecimal.s
@@ -1,12 +1,16 @@
         .data
 values: .word 128 64 32 16 8 4 2 1
-input:  .space 9
+input:  .space 9                        # Reserve 9 Bytes
 result: .word 0
 ans:    .asciiz "Der Wert ist "
 endl:   .asciiz "\n"
 		
         # t0 - Zum Zwischenspeichern eines Zeichens
-        # a1 - Zum Zwischenspeichern der aktuellen String-Postion
+        # a1 - Zum Zwischenspeichern der aktuellen String-Position
+        # a0 - Pointer to address of input string
+        # t5 - 0, t6 - 1
+        # t1 - current power of 2
+        # t4 - value of current power of 2
 
         .text
 main:
@@ -25,13 +29,10 @@ nextCh: # ==============================
         # Fügen Sie hier Ihre Lösung ein
         # ==============================
 
-
-
-
-
-
-
-		
+        lb      $t0, ($a0)      # Load Zeichen into t0
+        beqz    $t0, strEnd     # Jump to strEnd if end of string detected
+        # beq     $t0, $t5, null  # Jump if its zero
+        beq     $t0, $t6, eins  # Jump if its one
 
         # ==============================
         # Ende Ihrer Lösung
@@ -45,14 +46,10 @@ eins:				# Bit war 1
         # ==============================
         # Fügen Sie hier Ihre Lösung ein
         # ==============================
-
-
-
-
-
-
-		
-
+	
+	lw	$t4, ($t1)	# load value of current power of 2 into $t4
+        add     $t7, $t7, $t4   # Add t7 + t4 and store into t7
+        j       null            # Do what the null have to do
 
         # ==============================
         # Ende Ihrer Lösung
@@ -78,5 +75,5 @@ strEnd: li      $v0, 4 		# 4: print_str
 	la      $a0, endl       # Übergebe Adresse des Strings
 	syscall			# Gebe Newline aus
 
-        li      $v0,10
+        li      $v0, 10
         syscall                 # au revoir...
