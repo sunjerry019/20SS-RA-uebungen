d89fb9a6550cf441ae15a32c5a84ff29859e081c
Sun Yudong <sunjerry019@gmail.com>
2020-07-12 21:48:02 +0800

    Vorlage zur ha05a

---
+++ hausarbeit/ha05a/vorlage/vokale_ersetzen_oh.s
@@ -0,0 +1,90 @@
+.data
+needle: .asciiz "Rechnerarchitektur"
+result: .space 18
+ascii_vocals: .asciiz "aeiouAEIOU"
+string1: .asciiz "Rechnerarchitektur enthaelt: "
+string2: .asciiz " Vokale."
+string3: .asciiz "\nRechnerarchitektur ohne Vokale ist: "
+nline: .asciiz "\n"
+
+.text
+main:	li $t0, 0	# Zaehler fuer die Anzahl der Vokale
+	li $t1, 0	# Index des aktuell betrachteten Zeichens von needle
+	li $t2, 0	# Index des aktuell betrachteten Vokals in ascii_vocals
+	li $t3, 0	# Index des naechsten freien Speicherplatzes in result
+	
+
+n_loop: lb $t4, needle($t1)	
+	beqz $t4, end		
+	
+vocs:	####################################
+	# Fuegen Sie hier Ihre Loesung ein #
+	####################################
+	
+	
+	
+	
+	
+	
+	
+
+	
+	######################
+	# Ende Ihrer Loesung #
+	######################
+	j vocs			
+	
+vocal:	addi $t0, $t0, 1	
+	li $t2, 0		
+	addi $t1, $t1, 1	
+	j n_loop		
+
+
+# Der Coderahmen geht auf der folgenden Seite weiter!
+
+save: 	####################################
+	# Fuegen Sie hier Ihre Loesung ein #
+	####################################
+	
+	
+	
+	
+	
+	
+	
+	
+	######################
+	# Ende Ihrer Loesung #
+	######################
+	
+reset:  add $t1, $t1, 1		
+       	li $t2, 0		
+       	j n_loop		
+
+
+
+end:	li $v0, 4		
+	la $a0, string1		
+	syscall			
+	
+	li $v0, 1		
+	move $a0, $t0			
+	syscall			
+	
+	li $v0, 4		
+	la $a0, string2		
+	syscall			
+	
+	li $v0, 4		
+	la $a0, string3		
+	syscall			
+	
+	li $v0, 4		
+	la $a0, result		
+	syscall			
+	
+	li $v0, 10		
+	syscall			
+	
+	
+	
